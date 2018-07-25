b5d9787e42e2bcdc8795563227b06c403d0045e6
brutal-kitten <olgaprudyus@gmail.com>
2018-07-25 15:24:24 +0200

    feat: create file for bookshelf component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
+export Bookshelf from './Bookshelf'
 
 class BooksApp extends React.Component {
   state = {
@@ -10,7 +11,15 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+    showSearchPage: false,
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((allBooks) => {
+      console.log(allBooks);
+      this.setState({ books: allBooks })
+    })
   }
 
   render() {
---
+++ src/Bookshelf.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+
+class Bookshelf extends Component {
+
+  render() {
+
+  }
+}
+
+export default Bookshelf
